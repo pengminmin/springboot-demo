040ea0acdaab15c0975b1062b8a945e0226adf32
yuebuqun <540965717@qq.com>
2021-07-30 19:16:35 +0800

    rocketmq init

--- rocketmq/src/main/java/com/example/rocketmq/consumer/ConsumerApplication.java
+++ rocketmq/src/main/java/com/example/rocketmq/consumer/ConsumerApplication.java
@@ -1,6 +1,10 @@
 package com.example.rocketmq.consumer;
 
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 /**
  * @author yuebuqun
@@ -9,6 +13,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConsumerApplication {
     public static void main(String[] args) {
-        
+        SpringApplication.run(ConsumerApplication.class, args);
     }
+
+    @Service
+    @RocketMQMessageListener(topic = "test-topic-1", consumerGroup = "my-consumer-topic-1")
+    public class MyConsumer implements RocketMQListener<String> {
+
+        @Override
+        public void onMessage(String message) {
+            System.out.println("received message:" + message);
+        }
+    }
+
+
 }
